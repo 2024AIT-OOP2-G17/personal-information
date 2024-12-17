f1b2665ca6a94a10d8d43c4557a5a3f7bae84ce8
kakeru-sakai <k23053kk@aitech.ac.jp>
2024-12-17 19:41:02 +0900

    update:index

--- routes/index.py
+++ routes/index.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from models import Prefecture
+from routes import zodiac
 
 # Blueprintの作成
 index_bp = Blueprint('index', __name__, url_prefix='')
@@ -9,5 +10,13 @@ index_bp = Blueprint('index', __name__, url_prefix='')
 def list():
     # データ取得
     prefectures = Prefecture.select()
+    counts = zodiac.count_birthdays_by_month()
+    labels = [i for i in range(1, 13)]  # または list(range(1, 13))
+    data = [counts[month] for month in labels]
+    zodiac_cntdata = zodiac.count_zodiacs()
     
-    return render_template('index.html', prefectures = prefectures)
+    return render_template('index.html',
+                            prefectures = prefectures,
+                            labels=labels,
+                            data=data,
+                            zodiac_cntdata=zodiac_cntdata)
